54032672f151381eb342d7aa945cf73c122f98f4
ugurctaygun <ugurctaygun@gmail.com>
2020-04-24 20:46:01 +0300

    class, lifecycle hooks

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MovieList from './MovieList';
+import MovieCard from './MovieCard';
+
+function App() {
+  return (
+    <div className="App">
+    <MovieList />
+    <MovieCard />
+    </div>
+  );
+}
+
+export default App;
